d98ac0975452cce6c7f164ef254b409466d729b8
TaeBbong <mok05289@korea.ac.kr>
2023-04-24 23:02:28 +0900

    add: constants files

---
+++ lib/constants/colormap.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorMap {
+  static const red_heart = Color(0xffef5350);
+  static const black20 = Color(0x33000000);
+  static const black30 = Color(0x4c000000);
+  static const black40 = Color(0x66000000);
+  static const black50 = Color(0x80000000);
+  static const black80 = Color(0xcc000000);
+  static const white = Color(0xffffffff);
+  static const grey = Color(0xff828385);
+}
---
+++ lib/constants/text_styles.dart
@@ -0,0 +1,15 @@
+import 'package:aria_client/constants/colormap.dart';
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const splash_logo = TextStyle(
+      fontSize: 30, fontWeight: FontWeight.bold, color: ColorMap.black80);
+  static const title = TextStyle(
+      fontSize: 28, fontWeight: FontWeight.bold, color: ColorMap.white);
+  static const white_16_bold =
+      TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold);
+  static const white_16 = TextStyle(color: Colors.white, fontSize: 16);
+  static const white_24 = TextStyle(color: Colors.white, fontSize: 24);
+  static const white_30_bold =
+      TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold);
+}
