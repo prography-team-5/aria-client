221a35cb0b302ec4bfe02b419a1c3093a45b50df
SongheeNoh <s0n9h2@gmail.com>
2023-04-27 23:37:41 +0900

    feat: add colormap based on Figma

--- lib/constants/colormap.dart
+++ lib/constants/colormap.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ColorMap {
-  static const red_heart = Color(0xffef5350);
-  static const black20 = Color(0x33000000);
-  static const black30 = Color(0x4c000000);
-  static const black40 = Color(0x66000000);
-  static const black50 = Color(0x80000000);
-  static const black80 = Color(0xcc000000);
+  static const mainColor = Color(0xffff2768);
+  static const subColor = Color(0xff2d2942);
+  static const errorColor = Color(0xffff3541);
+  static const correctColor = Color(0xff29ee6c);
+  static const gray_700 = Color(0xff1b1a1a);
+  static const gray_600 = Color(0xff393737);
+  static const gray_500 = Color(0xff524f4f);
+  static const gray_400 = Color(0xff8d8989);
+  static const gray_300 = Color(0xffc6c0c0);
+  static const gray_200 = Color(0xffe4e2e2);
+  static const grey_100 = Color(0xfff4f2f2);
   static const white = Color(0xffffffff);
-  static const grey = Color(0xff828385);
 }
