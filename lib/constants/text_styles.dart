935c245d0108abae5ff616e2daa2ceaf9e1b5003
SongheeNoh <s0n9h2@gmail.com>
2023-04-28 00:03:17 +0900

    wip: Adjusting font size

--- lib/constants/text_styles.dart
+++ lib/constants/text_styles.dart
@@ -1,15 +1,28 @@
-import 'package:aria_client/constants/colormap.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TextStyles {
-  static const splash_logo = TextStyle(
-      fontSize: 30, fontWeight: FontWeight.bold, color: ColorMap.black80);
-  static const title = TextStyle(
-      fontSize: 28, fontWeight: FontWeight.bold, color: ColorMap.white);
-  static const white_16_bold =
-      TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold);
-  static const white_16 = TextStyle(color: Colors.white, fontSize: 16);
-  static const white_24 = TextStyle(color: Colors.white, fontSize: 24);
-  static const white_30_bold =
-      TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold);
+  // height: if want to have height 32.0px, with font-size 24.0px, we should have height property 1.2
+  // static final Heading1 = TextStyle(
+  //     fontFamily: 'PretendardBold', fontSize: 24.sp, height: 1.3, letterSpacing: -0.25);
+  static final Heading1 = TextStyle(
+      fontFamily: 'PretendardBold', fontSize: 24, height: 1.3, letterSpacing: -0.25);
+  static const Heading2 = TextStyle(
+      fontFamily: 'PretendardBold', fontSize: 20, height: 1.5, letterSpacing: -0.25);
+  static const Heading3 = TextStyle(
+      fontFamily: 'PretendardBold', fontSize: 16, height: 1.5, letterSpacing: -0.25);
+  static const Heading4 = TextStyle(
+      fontFamily: 'PretendardBold', fontSize: 14, height: 1.6, letterSpacing: -0.25);
+  static const Heading5 = TextStyle(
+      fontFamily: 'PretendardBold', fontSize: 13, height: 1.7, letterSpacing: -0.25);
+  static const Heading6 = TextStyle(
+      fontFamily: 'PretendardBold', fontSize: 10, height: 1.6, letterSpacing: -0.25);
+  static const Body1 = TextStyle(
+      fontSize: 16, height: 1.5, letterSpacing: -0.25);
+  static const Body2 = TextStyle(
+      fontSize: 16, fontWeight: FontWeight.w400, height: 1.5, letterSpacing: -0.25);
+  static const Caption = TextStyle(
+      fontSize: 13, fontWeight: FontWeight.w400, height: 1.5, letterSpacing: -0.25);
+  static const Badge = TextStyle(
+      fontSize: 12, fontWeight: FontWeight.w500, height: 1.5, letterSpacing: -0.25);
 }
--- lib/views/test/test_page.dart
+++ lib/views/test/test_page.dart
@@ -1,3 +1,4 @@
+import 'package:aria_client/constants/text_styles.dart';
 import 'package:flutter/material.dart';
 
 class TestPage extends StatelessWidget {
@@ -8,7 +9,20 @@ class TestPage extends StatelessWidget {
         title: Text('Test Page'),
       ),
       body: Center(
-        child: Text('Test Page'),
+        child: Column(
+          children: [
+            Text('안녕하세요.\nHello.', style: TextStyles.Heading1),
+            Text('안녕하세요.\nHello.', style: TextStyles.Heading2),
+            Text('안녕하세요.\nHello.', style: TextStyles.Heading3),
+            Text('안녕하세요.\nHello.', style: TextStyles.Heading4),
+            Text('안녕하세요.\nHello.', style: TextStyles.Heading5),
+            Text('안녕하세요.\nHello.', style: TextStyles.Heading6),
+            Text('안녕하세요.\nHello.', style: TextStyles.Body1),
+            Text('안녕하세요.\nHello.', style: TextStyles.Body2),
+            Text('안녕하세요.\nHello.', style: TextStyles.Caption),
+            Text('안녕하세요.\nHello.', style: TextStyles.Badge),
+          ],
+        ),
       ),
     );
   }
