085e8c03d27dbb0bf38cadb452b3f932ecc4ccc6
SongheeNoh <s0n9h2@gmail.com>
2023-05-15 21:56:27 +0900

    feat: signup page structure complete

--- lib/views/auth/signup_page.dart
+++ lib/views/auth/signup_page.dart
@@ -1,7 +1,17 @@
 import 'package:aria_client/constants/colormap.dart';
+import 'package:aria_client/constants/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+
+class GrayTextStyles {
+  static final Body2 = TextStyle(
+      color: ColorMap.gray_300,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      height: 1.5,
+      letterSpacing: -0.25);
+}
 
 class SignupPage extends StatelessWidget {
   @override
@@ -10,6 +20,7 @@ class SignupPage extends StatelessWidget {
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(56),
         child: AppBar(
+          automaticallyImplyLeading: false,
           title: Text(
             '회원가입',
             style: TextStyle(
@@ -17,12 +28,8 @@ class SignupPage extends StatelessWidget {
                 fontSize: 16,
                 fontWeight: FontWeight.w500),
           ),
-          // leading: SvgPicture.asset(
-          //   'assets/images/leading_button.svg',
-          //   fit: BoxFit.cover,
-          // ),
           leading: Padding(
-            padding: EdgeInsets.fromLTRB(24, 16, 0, 16),
+            padding: EdgeInsets.fromLTRB(12, 16, 0, 16),
             child: GestureDetector(
               onTap: () => Get.back(),
               child: SvgPicture.asset(
@@ -30,13 +37,65 @@ class SignupPage extends StatelessWidget {
               ),
             ),
           ),
-          centerTitle: true, // 가운데 정렬
+          centerTitle: true,
           backgroundColor: Colors.transparent,
           elevation: 0.0, // appBar 그림자 제거
         ),
       ),
-      extendBodyBehindAppBar: true, // body 위에 appbar
-      body: Container(),
+      body: Container(
+        margin: EdgeInsets.fromLTRB(24, 24, 24, 12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '닉네임',
+              style: TextStyles.Heading3,
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  borderSide: BorderSide(color: ColorMap.gray_200),
+                ),
+                hintText: '아리아에서 사용할 닉네임을 입력해주세요.',
+                hintStyle: GrayTextStyles.Body2,
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+              ),
+            ),
+            const Spacer(),
+            SizedBox(
+              width: double.infinity,
+              height: 64,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  '회원가입',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    height: 1.375,
+                  ),
+                ),
+                style: TextButton.styleFrom(
+                  // TODO : controller 추가, 비활성 버튼 색 변경
+                  backgroundColor: ColorMap.mainColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
