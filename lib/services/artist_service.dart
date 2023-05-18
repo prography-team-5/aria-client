019cca0aa4f507bc307668f74b5b87c3d3d18dfb
TaeBbong <mok05289@korea.ac.kr>
2023-05-18 15:15:29 +0200

    feat: Fake ArtistInfo Service

--- lib/services/artist_service.dart
+++ lib/services/artist_service.dart
@@ -1,6 +1,56 @@
 // fetchArtist()
 // fetchArtists()
 
+import 'package:aria_client/helpers/env.dart';
+import 'package:aria_client/models/artist_info.dart';
 import 'package:get/get.dart';
 
-class ArtistService extends GetxService {}
+class ArtistService extends GetxService {
+  Future<ArtistInfo?> fetchArtist() async {
+    if (Env.env == Environ.dev) {
+      return ArtistInfo(
+        id: 1,
+        member_id: 1,
+        profile_art_image_url: 'https://picsum.photos/200/300',
+        intro: 'Artist Intro',
+      );
+    } else {
+      // TODO: fetch from API
+      return null;
+    }
+  }
+
+  Future<List<ArtistInfo>?> fetchArtists() async {
+    if (Env.env == Environ.dev) {
+      return [
+        ArtistInfo(
+          id: 1,
+          member_id: 1,
+          profile_art_image_url: 'https://picsum.photos/200/300',
+          intro: 'Artist Intro',
+        ),
+        ArtistInfo(
+          id: 2,
+          member_id: 1,
+          profile_art_image_url: 'https://picsum.photos/200/300',
+          intro: 'Artist Intro',
+        ),
+        ArtistInfo(
+          id: 3,
+          member_id: 1,
+          profile_art_image_url: 'https://picsum.photos/200/300',
+          intro: 'Artist Intro',
+        ),
+        ArtistInfo(
+          id: 4,
+          member_id: 1,
+          profile_art_image_url: 'https://picsum.photos/200/300',
+          intro: 'Artist Intro',
+        ),
+      ];
+    } else {
+      // TODO: fetch from API
+      return null;
+    }
+  }
+}
