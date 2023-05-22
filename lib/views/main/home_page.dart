bcfc96d71b5a93f063bb970dc21e4f0fc9f80a2f
SongheeNoh <s0n9h2@gmail.com>
2023-05-22 20:45:54 +0900

    wip: making AppBar

--- lib/views/main/home_page.dart
+++ lib/views/main/home_page.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(56),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          leading: Padding(
+            padding: EdgeInsets.fromLTRB(12, 16, 0, 16),
+            child: SvgPicture.asset(
+              'assets/images/logo.svg',
+            ),
+          ),
+          actions: [
+            GestureDetector(
+              onTap: () => Get.toNamed('/search'),
+              child: SvgPicture.asset(
+                'assets/images/search_button.svg',
+              ),
+            ),
+            GestureDetector(
+              onTap: () => Get.toNamed('/my'),
+              child: SvgPicture.asset(
+                'assets/images/my_button.svg',
+              ),
+            ),
+          ],
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0.0, // appBar 그림자 제거
+        ),
+      ),
       body: Container(),
     );
   }
