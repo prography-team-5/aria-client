8ef7beac2a67b2d86a62c58ad2c117a4f0c63b22
TaeBbong <mok05289@korea.ac.kr>
2023-05-31 19:32:14 +0900

    wip: auth service

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,4 +3,10 @@
 
 import 'package:get/get.dart';
 
-class AuthService extends GetxService {}
+class AuthService extends GetxService {
+  Future<void> signIn() async {}
+  Future<void> signUp() async {}
+  Future<void> signUpWithGoogle() async {}
+  Future<void> signUpWithKaKao() async {}
+  Future<void> signUpWithApple() async {}
+}
