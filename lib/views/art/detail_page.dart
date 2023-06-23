fa22c2f53da0afec5a6d62de3a8a050f79370e44
SongheeNoh <s0n9h2@gmail.com>
2023-06-24 03:34:51 +0900

    wip: add scrollview and apply bottom blur effect

--- lib/views/art/detail_page.dart
+++ lib/views/art/detail_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      body: Container(
+        child: SvgPicture.asset(
+          'assets/images/logo_white.svg',
+          alignment: Alignment.center,
+          fit: BoxFit.fill,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+        ),
+      ),
     );
   }
 }
