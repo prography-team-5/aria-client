c6ba3f9f3308f8a724bc527b89ce9bd683b930c0
TaeBbong <mok05289@korea.ac.kr>
2023-07-10 22:32:12 +0900

    chore: merge conflicts

--- lib/views/main/home_page.dart
+++ lib/views/main/home_page.dart
@@ -1,86 +1,108 @@
+import 'package:aria_client/constants/colormap.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart' as svg_provider;
 
 import '../../viewmodels/main/home_viewmodel.dart';
 
 class HomePage extends GetView<HomeViewModel> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(56),
-        child: AppBar(
-          automaticallyImplyLeading: false,
-          leading: Padding(
-            padding: EdgeInsets.fromLTRB(12, 16, 0, 16),
-            child: SvgPicture.asset(
-              'assets/images/logo.svg',
-            ),
-          ),
-          actions: [
-            GestureDetector(
-              onTap: () => Get.toNamed('/search'),
-              child: SvgPicture.asset(
-                'assets/images/search_button.svg',
-              ),
-            ),
-            GestureDetector(
-              onTap: () => Get.toNamed('/my'),
-              child: SvgPicture.asset(
-                'assets/images/my_button.svg',
-              ),
-            ),
-          ],
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
-          elevation: 0.0, // appBar 그림자 제거
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: svg_provider.Svg('assets/image_example.svg'),
         ),
       ),
-      body: Column(children: [
-        Padding(padding: EdgeInsets.all(20)),
-        Text(
-          controller.signinViewModel.member!.nickname,
-          style: TextStyle(color: Colors.black),
-        ),
-        Padding(padding: EdgeInsets.all(20)),
-        Row(
-          children: [
-            Flexible(
-              child: Text(
-                'AccessToken : ' + controller.signinViewModel.accessToken,
-                style: TextStyle(color: Colors.black),
-                softWrap: true,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(56),
+          child: AppBar(
+            automaticallyImplyLeading: false,
+            leading: Padding(
+              padding: EdgeInsets.fromLTRB(12, 16, 0, 16),
+              child: SvgPicture.asset(
+                'assets/images/logo.svg',
               ),
             ),
-            TextButton(
-                onPressed: () {
-                  Clipboard.setData(ClipboardData(
-                      text: controller.signinViewModel.accessToken));
-                },
-                child: Text('Copy'))
-          ],
-        ),
-        Padding(padding: EdgeInsets.all(20)),
-        Row(
-          children: [
-            Flexible(
-              child: Text(
-                'RefreshToken : ' + controller.signinViewModel.refreshToken,
-                style: TextStyle(color: Colors.black),
-                softWrap: true,
+            actions: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 12, 0),
+                child: GestureDetector(
+                  onTap: () => Get.toNamed('/search'),
+                  child: SvgPicture.asset(
+                    'assets/images/search_button.svg',
+                  ),
+                ),
               ),
-            ),
-            TextButton(
-                onPressed: () {
-                  Clipboard.setData(ClipboardData(
-                      text: controller.signinViewModel.refreshToken));
-                },
-                child: Text('Copy'))
-          ],
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 12, 0),
+                child: GestureDetector(
+                  onTap: () => Get.toNamed('/my'),
+                  child: SvgPicture.asset(
+                    'assets/images/my_button.svg',
+                  ),
+                ),
+              ),
+            ],
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0.0, // appBar 그림자 제거
+          ),
         ),
-      ]),
+        body: Container(
+          margin: const EdgeInsets.fromLTRB(24, 0, 24, 0),
+          child: Column(
+            children: [
+              Spacer(),
+              SizedBox(
+                width: 342,
+                height: 592,
+                child: Container(color: Colors.white),
+              ),
+              Spacer(),
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: SvgPicture.asset(
+                      'assets/images/share_button.svg',
+                    ),
+                    style: TextButton.styleFrom(
+                      backgroundColor: ColorMap.subColor,
+                      shape: const CircleBorder(),
+                      fixedSize: Size(64, 64),
+                    ),
+                  ),
+                  SizedBox(width: 9),
+                  SizedBox(
+                    width: 256, // TODO: flexible하게 바꿀 것
+                    height: 64,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: FittedBox(
+                        child: Text('전시회 방문하기',
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                      style: TextButton.styleFrom(
+                        backgroundColor: ColorMap.mainColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                        // fixedSize: Size(double.infinity, 64),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 40)
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
