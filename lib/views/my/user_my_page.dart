3fe8c4f05c724a1681c8f62764c72f78633d6d7e
TaeBbong <mok05289@korea.ac.kr>
2023-07-21 00:05:42 +0900

    wip: mypage ui

--- lib/views/my/user_my_page.dart
+++ lib/views/my/user_my_page.dart
@@ -3,10 +3,13 @@ import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 
 import '../../constants/colormap.dart';
+import '../../constants/text_styles.dart';
+import '../../viewmodels/auth/signin_viewmodel.dart';
 
 class UserMyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final signinViewModel = Get.find<SigninViewModel>();
     return SafeArea(
       child: Scaffold(
         appBar: PreferredSize(
@@ -37,7 +40,7 @@ class UserMyPage extends StatelessWidget {
                 child: TextButton(
                   onPressed: () {},
                   child: Text(
-                    '작품 등록',
+                    '작가 신청',
                     style: TextStyle(
                       color: ColorMap.white,
                       fontFamily: 'Prentendard',
@@ -60,7 +63,63 @@ class UserMyPage extends StatelessWidget {
             elevation: 0.0, // appBar 그림자 제거
           ),
         ),
-        body: Container(),
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            Column(
+              children: [
+                Container(
+                  height: 400,
+                  child: Image.asset(
+                    'assets/images/profile_background.png',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Expanded(
+                  // height: 200,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 30 + 5),
+                    child: Column(
+                      children: [
+                        Text(
+                          signinViewModel.member!.nickname,
+                          style: TextStyles.Heading2,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            '프로필 수정',
+                            style: TextStyle(color: ColorMap.gray_700),
+                          ),
+                          style: TextButton.styleFrom(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 130, vertical: 13),
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                  color: ColorMap.gray_200, width: 1),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              top: 400 - 50,
+              child: Container(
+                height: 100.0,
+                width: 100.0,
+                child: Image.asset('assets/images/profile_avatar.png'),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
